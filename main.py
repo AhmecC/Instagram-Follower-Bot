ae9e0b0dcf655a368e3935554570c89b3ab46ccb
Aqeel Ahmed <aqeelvahmed@gmail.com>
2022-07-21 16:16:50 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from the_rest import FollowFollowers
+
+# -------------------- CONSTANTS --------------------
+PATH = "YOUR_CHROMEDRIVER_PATH"
+TARGET = "Target Account Name"
+USER = "Instagram Username"
+PASS = "Instagram Password"
+
+
+Bot = FollowFollowers(PATH, TARGET, USER, PASS)
